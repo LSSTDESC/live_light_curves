4a261163bc87ebe5864bc68fa7a62525974b1fee
Henry-Best-01 <henrygamemaster@aol.com>
2026-06-09 14:01:11 +0200

    last check before running lightcurver

--- src/livelcs/Pipeline/live_light_curves.py
+++ src/livelcs/Pipeline/live_light_curves.py
@@ -141,21 +141,24 @@ for jj in tqdm.tqdm(range(len(targets))):
                 verbose=True
             )
 
-            # Lightcurver requires this main wrapper on some systems.
-            if __name__ == '__main__':
-                get_user_config()
-                initialize_database()
-                read_convert_skysub_character_catalog()
-                plate_solve_all_frames() 
-                calc_common_and_total_footprint_and_save()
-                query_gaia_stars()
-                extract_all_stamps()
-                model_all_psfs()
-                do_star_photometry()
-                calculate_coefficient()
-                calculate_zeropoints()
-                prepare_roi_file()
-                do_modelling_of_roi()   
+
+            if len(written_files) > 0:
+
+                # Lightcurver requires this main wrapper on some systems.
+                if __name__ == '__main__':
+                    get_user_config()
+                    initialize_database()
+                    read_convert_skysub_character_catalog()
+                    plate_solve_all_frames()
+                    calc_common_and_total_footprint_and_save()
+                    query_gaia_stars()
+                    extract_all_stamps()
+                    model_all_psfs()
+                    do_star_photometry()
+                    calculate_coefficient()
+                    calculate_zeropoints()
+                    prepare_roi_file()
+                    do_modelling_of_roi()
 
 
 
