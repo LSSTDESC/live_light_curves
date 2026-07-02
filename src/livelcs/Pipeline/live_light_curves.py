fdb1e71c8cf4a1d1163ed8722a3f70e5148d8540
Henry-Best-01 <henrygamemaster@aol.com>
2026-07-02 09:56:19 -0400

    printing a test

--- src/livelcs/Pipeline/live_light_curves.py
+++ src/livelcs/Pipeline/live_light_curves.py
@@ -83,6 +83,8 @@ if type(targets) != DataFrame:
 
 for jj in tqdm.tqdm(range((targets.shape[0]))):
 
+    print(targets.iloc[jj])
+
     working_data = targets.iloc[jj]
 
     target_string, ra, dec = extract_ra_dec_target_string(working_data)
