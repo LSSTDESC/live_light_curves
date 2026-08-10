80923d5ef8602a996d8323954deb9c8434d3e141
Henry-Best-01 <henrygamemaster@aol.com>
2026-08-10 10:06:50 -0400

    seperating data by band

--- src/livelcs/Util/util.py
+++ src/livelcs/Util/util.py
@@ -338,8 +338,7 @@ def make_temp_yaml_with_new_roi(target, original_path, band, extension="_tmp"):
             current_line = file.readline()
             if current_line.startswith('raw_dirs:'):
                 raw_dir_str = current_line.split(sep=':')
-                raw_dir = (raw_dir_str[1].strip())
-                print(raw_dir)
+                raw_dir = raw_dir_str[1].strip()+f'{band}/'
             if current_line == 'photometric_band:\n':
                 if band in ['u', 'g']: approx_band = "g_sdss"
                 elif band == ['r']: approx_band = "r_sdss"
