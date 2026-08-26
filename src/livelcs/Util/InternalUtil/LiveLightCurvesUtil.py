aae6e3a33ef69b7b1051f6f7fe67e824a4c7a71a
Henry-Best-01 <henrygamemaster@aol.com>
2026-08-26 15:18:31 -0400

    bugsplat

--- src/livelcs/Util/InternalUtil/LiveLightCurvesUtil.py
+++ src/livelcs/Util/InternalUtil/LiveLightCurvesUtil.py
@@ -103,7 +103,7 @@ def load_light_curve(file_name, directory="./extracted_light_curves/", extension
         os.mkdir(directory)
     if os.path.isfile(os.path.join(directory+file_name)+extension) is False:
         blank_light_curve = LightCurve()
-        blank_light_curve.save_light_curve(os.path.join(directory+file_name), extension=extension)
+        blank_light_curve.save_light_curve(file_name, directory=directory, extension=extension)
     with open(os.path.join(directory,file_name)+extension, 'r') as f:
         loaded_light_curve_data = json.load(f)
         loaded_light_curve = LightCurve(data=loaded_light_curve_data)
